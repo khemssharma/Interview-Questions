c9a851cb7d48f931f77df90c5afbc46ed8e09851
Ayush Sharma <ayushsharma3310@gmail.com>
2024-10-21 17:55:15 +0530

    update array.js

--- array.js
+++ array.js
@@ -28,3 +28,30 @@ var maxArea = function (height) {
     }
     return maxWater
 };
+
+// trapping rain water
+var trap = function(height) {
+    if (height.length === 0) return 0;
+    // two pointer and correspoding variables
+    let left = 0, leftMax = 0;
+    let right = height.length - 1, rightMax = 0;
+    let waterTrapped = 0;
+    while (left < right){
+        if (height[left] < height[right]){
+            if (height[left] >= leftMax){
+                leftMax = height[left];
+            }else{
+                waterTrapped += leftMax - height[left]
+            }
+            left++;
+        }else{
+            if (height[right] >= rightMax){
+                rightMax = height[right];
+            }else{
+                waterTrapped += rightMax - height[right]
+            }
+            right--;
+        }
+    }
+    return waterTrapped;
+};
