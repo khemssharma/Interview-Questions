a22bbe0e0ed6356922bab0a7e0ab751537089579
Ayush Sharma <ayushsharma3310@gmail.com>
2024-10-22 18:18:46 +0530

    update array.js

--- array.js
+++ array.js
@@ -41,6 +41,36 @@ var searchInsert = function (nums, target) {
     return left;
 };
 
+var searchRange = function (nums, target) {
+    const binarySearch = (isleft) => {
+        let left = 0,
+            right = nums.length - 1;
+        let index = -1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                index = mid;
+                if (isleft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return index;
+    }
+    const start = binarySearch(true);
+    if (start === -1) {
+        return [-1, -1];
+    }
+    const end = binarySearch(false);
+    return [start, end];
+};
+
 // container with most water
 var maxArea = function (height) {
     // two pointers
