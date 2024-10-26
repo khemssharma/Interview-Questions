ad81f5e28cfb7cc6579ab40ee485a3e83d71b40f
Ayush Sharma <ayushsharma3310@gmail.com>
2024-10-26 21:58:09 +0530

    update 26 oct

--- array.js
+++ array.js
@@ -3,6 +3,48 @@
  * @return {number}
  */
 
+var twoSum = function(nums, target) {                               // assume nums [3,2,4] target 6
+    const map = new Map();                                          // create a new hashmap
+    for (let i = 0; i < nums.length ; i++){                         // now iterate over nums
+        const complement = target - nums[i]                         // find complement=target-current. for 3 = 3
+        if (map.has(complement)){                                   // if map has 3
+            return [map.get(complement), i]                         // return index of 3, and i = 0
+        }                                                           // well it has not 3 so 
+        map.set(nums[i], i)                                         // set current number to the map
+    }
+}; 
+
+var missingNumber = function(nums) {
+    const n = nums.length;
+    const expextedSum = (n*(n+1))/2;
+    const actualSum = nums.reduce((sum, num)=>num+sum, 0)
+    return expextedSum-actualSum;
+};
+
+function singleNumber(nums) {
+    let unique = 0;
+    for (let num of nums) {
+        unique ^= num;  // XOR each number
+    }
+    return unique;
+}
+
+function moveZeroes(nums) {
+    let lastNonZeroIndex = 0;
+    
+    // Move all non-zero elements to the front
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[lastNonZeroIndex] = nums[i];
+            lastNonZeroIndex++;
+        }
+    }
+    // Fill the rest of the array with zeros
+    for (let i = lastNonZeroIndex; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+}
+
 var check = function (nums) { // check if array is sorted (increasing) and rotated
     // Simply check if the next number is greater than the current
     // If the array is sorted in cyclic order then the next is nums[(i+1)%nums.length]
@@ -66,8 +108,8 @@ var removeElement = function (nums, val) { // remove element by value
 
 var rotate = function (matrix) { // rotate matrix 90 degree
     let n = matrix.length;
-    for (let i=0; i<n; i++) {
-        for (let j = i + 1; j < n; j++) {   //  j is always 1 more to i to avoid unnecessary swap (overriding number itself)
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) { //  j is always 1 more to i to avoid unnecessary swap (overriding number itself)
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
@@ -86,7 +128,7 @@ var maxArea = function (height) { // container with most water
         const area = Math.min(height[left], height[right]) * (right - left) // calculate area enclosed by left and right pillar
         maxWater = Math.max(maxWater, area) // if current area is maximum, update it 
         if (height[left] < height[right]) { // continue with next larger pillar whether it is left or right 
-            left++; 
+            left++;
         } else {
             right--;
         }
@@ -124,6 +166,38 @@ var firstMissingPositive = function (nums) { // first missing positive
         }
     }
     return n + 1; // If all numbers are at their correct position return i+1
+};
+
+var spiralOrder = function (matrix) {
+    const result = [];
+    if (matrix.length == 0) return result;
+    let top = 0,
+        bottom = matrix.length - 1;
+    let left = 0,
+        right = matrix[0].length - 1;
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
+        right--;
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+        }
+        bottom--;
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+        }
+        left++;
+    }
+    return result;
 };
 
 var trap = function (height) { // trapping rain water
